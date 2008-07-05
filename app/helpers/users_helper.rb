a75e629257586adff6ea7cebdc91cdda245495aa
David Hayward <spidahman@gmail.com>
2008-07-05 00:22:01 +0000

    I hate timezones, so going back to the retarded Rails ones as it's a bitch to deal with otherwise.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,2 @@
 module UsersHelper
-  def time_zone_options(selected = nil)
-    zone_options = ""
-
-    zones = TZInfo::Timezone.all
-    convert_zones = lambda { |list| list.map { |z| [ z.identifier, z.to_s ] } }
-
-    zone_options += options_for_select(convert_zones[zones], selected)
-    zone_options
-  end
 end
