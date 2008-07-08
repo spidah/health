2145522b5d7787aaec63047ff33d5470dd2f06bd
David Hayward <spidahman@gmail.com>
2008-07-08 11:08:39 +0000

    Protect the controller and set the menu item.

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,2 +1,9 @@
 class CalendarController < ApplicationController
+  before_filter :login_required, :set_menu_item
+
+
+  protected
+    def set_menu_item
+      @activemenuitem = 'menu-account'
+    end
 end
