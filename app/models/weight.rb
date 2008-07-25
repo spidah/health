363c1554a0cbea1c41643e4b75194e91aff32b40
David Hayward <spidahman@gmail.com>
2008-07-25 00:12:39 +0000

    Allow getting a weight for a single date.

--- app/models/weight.rb
+++ app/models/weight.rb
@@ -38,6 +38,10 @@ class Weight < ActiveRecord::Base
     find(:first, :order => 'taken_on DESC')
   end
 
+  def self.get_for_date(date)
+    find(:first, :conditions => {:taken_on => date})
+  end
+
   def self.get_count(date)
     count('id', :conditions => "taken_on = '#{date}'")
   end
