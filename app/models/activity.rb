f95aaacbc71ca07c51a73a73ef6da226c5ac9358
David Hayward <spidahman@gmail.com>
2008-07-30 23:57:02 +0000

    * Use attr_accessor to only allow the given attributes to be changed.  * Added validation calls.  * Added a pagination method for use in the controller.  * Changed the inheritance_column to 'unused' so that the type column can actually be used.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,18 @@
 class Activity < ActiveRecord::Base
   belongs_to :user
+  self.inheritance_column = 'unused'
+
+  # only allow these attributes to be changeable
+  attr_accessible :name, :type, :duration, :calories
+
+  validates_presence_of :name, :message => 'Please enter a name for the activity.'
+  validates_presence_of :type, :message => 'Please select a type for the activity.'
+  validates_presence_of :duration, :message => 'Please enter a duration for the activity.'
+  validates_presence_of :calories, :message => 'Please enter the calories for the activity.'
+
+  def self.pagination(page, sort = nil, dir = 'ASC')
+    paginate :page => page, :per_page => 50, :order => sort ? "#{sort} #{dir}" : 'id ASC'
+  end
 
   protected
     def after_find
@@ -10,5 +23,10 @@ class Activity < ActiveRecord::Base
     def before_save
       self[:duration] *= 100
       self[:calories] *= 100
+    end
+
+    def validate
+      errors.add(:duration, 'Please enter a valid duration for the activity.') if self[:duration].to_i <= 0
+      errors.add(:calories, 'Please enter a valid calorie count for the activity.') if self[:calories].to_i <= 0
     end
 end
