be8ed1f91d8aa2e1f386394cbc0d6c91e8297fde
David Hayward <spidahman@gmail.com>
2008-07-30 23:46:46 +0000

    Forgot the user_id column.

--- db/migrate/20080729201122_create_exercise_tables.rb
+++ db/migrate/20080729201122_create_exercise_tables.rb
@@ -1,13 +1,14 @@
 class CreateExerciseTables < ActiveRecord::Migration
   def self.up
     create_table :exercises do |t|
-      t.integer       :activity_id
+      t.integer       :user_id, :activity_id
       t.string        :activity_name, :activity_type
       t.integer       :duration, :calories,                   :default => 0
       t.date          :taken_on
     end
 
     create_table :activities do |t|
+      t.integer       :user_id
       t.string        :name, :type
       t.integer       :duration, :calories,                   :default => 0
     end
