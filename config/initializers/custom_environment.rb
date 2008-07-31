263773376df22f089b805cd970886ef887289de1
David Hayward <spidahman@gmail.com>
2008-07-31 23:37:48 +0000

    Added min and max methods to the Math module.

--- config/initializers/custom_environment.rb
+++ config/initializers/custom_environment.rb
@@ -89,6 +89,17 @@ class String #:nodoc:
   include MyExtensions::CoreExtensions::String::TextHelper
 end
 
+# give the Math module min and max methods
+module Math
+  def self.min(a, b)
+    a < b ? a : b
+  end
+
+  def self.max(a, b)
+    a > b ? a : b
+  end
+end  
+
 def logger
   RAILS_DEFAULT_LOGGER
 end
