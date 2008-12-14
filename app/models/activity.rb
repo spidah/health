5abda3b41831ac738c0da7535369d9f764a34250
David Hayward <spidahman@gmail.com>
2008-12-14 19:10:39 +0000

    Format changes.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -17,13 +17,14 @@ class Activity < ActiveRecord::Base
   end
 
   protected
-    def after_find
-      self[:duration] /= 100 if self[:duration]
-      self[:calories] /= 100 if self[:calories]
-    end
 
-    def before_save
-      self[:duration] *= 100
-      self[:calories] *= 100
-    end
+  def after_find
+    self[:duration] /= 100 if self[:duration]
+    self[:calories] /= 100 if self[:calories]
+  end
+
+  def before_save
+    self[:duration] *= 100
+    self[:calories] *= 100
+  end
 end
