9fc37d70d5898cbe72dc3fcc1e1562eba7b7dd63
David Hayward <spidahman@gmail.com>
2008-12-16 23:50:10 +0000

    Format changes.

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -4,19 +4,20 @@ class FoodItem < ActiveRecord::Base
   validates_numericality_of :quantity, :only_integer => true, :greater_than => 0, :message => 'You need a quantity of at least 1.'
 
   protected
-    def after_destroy
-      meal.update_calories
-    end
 
-    def after_find
-      self[:calories] = self[:calories] / 100
-    end
+  def after_destroy
+    meal.update_calories
+  end
 
-    def after_save
-      meal.update_calories
-    end
+  def after_find
+    self[:calories] = self[:calories] / 100
+  end
 
-    def before_save
-      self[:calories] = self[:calories] * 100
-    end
+  def after_save
+    meal.update_calories
+  end
+
+  def before_save
+    self[:calories] = self[:calories] * 100
+  end
 end
