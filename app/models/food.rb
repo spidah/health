55fae7ce0f6913fd56a11eb693e6717148d208e1
David Hayward <spidahman@gmail.com>
2008-12-17 17:57:08 +0000

    Food unit test.
     * Added getter and setter methods for the fat, protein, carbs and calories attributes, to handle the multiplication and division.
     * Removed the defunct after_find and before_save methods.
     * This commit currently breaks other tests as it doesn't set the @change value, as this is going to be rewritten to use dirty records stuff.

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,53 +2,52 @@ class Food < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :name, :message => 'Please enter a name for the food.'
+  validates_numericality_of :fat, :only_integer => true, :greater_than_or_equal_to => 0, :message => 'Please enter a valid fat value.'
+  validates_numericality_of :protein, :only_integer => true, :greater_than_or_equal_to => 0, :message => 'Please enter a valid protein value.'
+  validates_numericality_of :carbs, :only_integer => true, :greater_than_or_equal_to => 0, :message => 'Please enter a valid carbohydrate value.'
+  validates_numericality_of :calories, :only_integer => true, :greater_than_or_equal_to => 0, :message => 'Please enter a valid calories value.'
 
   def self.pagination(page, sort = nil, dir = 'ASC')
     paginate :page => page, :per_page => 50, :order => sort ? "#{sort} #{dir}" : 'id ASC'
   end
 
   def fat
-    fat_before_type_cast
+    @fat ||= self[:fat] / 100
+  end
+
+  def fat=(value)
+    self[:fat] = value.to_i * 100
+    @fat = value.to_i
   end
 
   def protein
-    protein_before_type_cast
+    @protein ||= self[:protein] / 100
+  end
+
+  def protein=(value)
+    self[:protein] = value.to_i * 100
+    @protein = value.to_i
   end
 
   def carbs
-    carbs_before_type_cast
+    @carbs ||= self[:carbs] / 100
   end
 
-  def calories=(new_calories)
-    @changed = true if new_calories.to_i != self[:calories]
-    self[:calories] = new_calories
+  def carbs=(value)
+    self[:carbs] = value.to_i * 100
+    @carbs = value.to_i
   end
 
-  def name=(new_name)
-    @changed = true if new_name != self[:name]
-    self[:name] = new_name
+  def calories
+    @calories ||= self[:calories] / 100
   end
 
-  def description=(new_description)
-    @changed = true if new_description != self[:description]
-    self[:description] = new_description
+  def calories=(value)
+    self[:calories] = value.to_i * 100
+    @calories = value.to_i
   end
 
   protected
-    def after_find
-      self[:fat] = (self[:fat].to_f / 100)
-      self[:protein] = (self[:protein].to_f / 100)
-      self[:carbs] = (self[:carbs].to_f / 100)
-      self[:calories] = self[:calories] / 100
-    end
-
-    def before_save
-      self[:fat] = (fat_before_type_cast.to_f * 100).to_i
-      self[:protein] = (protein_before_type_cast.to_f * 100).to_i
-      self[:carbs] = (carbs_before_type_cast.to_f * 100).to_i
-      self[:calories] = self[:calories] * 100
-    end
-
     def after_save
       if @changed
         @items = FoodItem.find(:all, :conditions => {:food_id => self[:id]})
--- test/unit/food_test.rb
+++ test/unit/food_test.rb
@@ -1,8 +1,67 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
-class FoodTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+class FoodTest < Test::Unit::TestCase
+  def setup
+    @user = User.find(users(:spidah).id)
+    @valid_attributes = { :name => 'Food', :description => 'Food', :manufacturer => 'Shop', :weight => '1', :fat => 1, :protein => 1, :carbs => 1, :calories => 1 }
+  end
+
+  def print_errors(food)
+    food.errors.full_messages.to_sentence
+  end
+
+  def test_should_create
+    assert_difference(Food, :count) do
+      food = @user.foods.create(@valid_attributes)
+      assert(!food.new_record?, print_errors(food))
+    end
+  end
+
+  def test_should_require_name
+    assert_no_difference(Food, :count) do
+      food = @user.foods.create(@valid_attributes.except(:name))
+      assert(food.new_record?)
+      assert(food.errors.on(:name))
+    end
+  end
+
+  def test_should_require_valid_attributes
+    [:fat, :protein, :carbs, :calories].each { |attribute|
+      assert_no_difference(Food, :count) do
+        food = @user.foods.create(@valid_attributes.merge(attribute => -1))
+        assert(food.new_record?)
+        assert(food.errors.on(attribute))
+      end
+    }
+  end
+
+  def test_should_update
+    food = @user.foods.create(@valid_attributes)
+    assert_equal('Food', food.name)
+    assert_equal('Food', food.description)
+    assert_equal('Shop', food.manufacturer)
+    assert_equal('1', food.weight)
+    assert_equal(1, food.fat)
+    assert_equal(1, food.protein)
+    assert_equal(1, food.carbs)
+    assert_equal(1, food.calories)
+
+    food.update_attributes(:name => 'Not Food', :description => 'Not Food', :manufacturer => 'Not Shop', :weight => '2', :fat => 2, :protein => 2, :carbs => 2, :calories => 2)
+    food.reload
+
+    assert_equal('Not Food', food.name)
+    assert_equal('Not Food', food.description)
+    assert_equal('Not Shop', food.manufacturer)
+    assert_equal('2', food.weight)
+    assert_equal(2, food.fat)
+    assert_equal(2, food.protein)
+    assert_equal(2, food.carbs)
+    assert_equal(2, food.calories)
+  end
+
+  def test_should_destroy
+    food = @user.foods.create(@valid_attributes)
+    food.destroy
+    assert_raise(ActiveRecord::RecordNotFound) { Food.find(food.id) }
   end
 end
