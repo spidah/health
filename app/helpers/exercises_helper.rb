25bad644316506d9fd987011d76ce22f61223c34
David Hayward <spidahman@gmail.com>
2009-02-26 20:20:29 +0000

    Make sure there's a space before the selected attribute.

--- app/helpers/exercises_helper.rb
+++ app/helpers/exercises_helper.rb
@@ -1,5 +1,5 @@
 module ExercisesHelper
   def activities_to_options(activities, id)
-    activities.collect {|activity| "<option value=\"#{activity.id}\"#{"selected=\"selected\"" if id == activity.id}>#{activity.name}#{' - ' + activity.description if !activity.description.blank?}</option>"}
+    activities.collect {|activity| "<option value=\"#{activity.id}\"#{" selected=\"selected\"" if id == activity.id}>#{activity.name}#{' - ' + activity.description if !activity.description.blank?}</option>"}
   end
 end
