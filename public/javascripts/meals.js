99286a00e06c81bbee6c4dae42b3bcc7c5d8796d
David Hayward <spidahman@gmail.com>
2009-03-12 15:24:31 +0000

    The inputs now have a span tag around them, meaning that an extra parent() call is needed to get to the form.

--- public/javascripts/meals.js
+++ public/javascripts/meals.js
@@ -8,10 +8,10 @@ $(function() {
   };
 
   $('table#meals tr td form input').live('click', function() {
-    var row = $(this).parent().parent().parent().get(0);
+    var row = $(this).parent().parent().parent().parent().get(0);
     $.ajax({
       type: 'PUT',
-      url: $($(this).parent().get(0)).attr('action'),
+      url: $($(this).parent().parent().get(0)).attr('action'),
       data: "authenticity_token=" + encodeURIComponent(AUTH_TOKEN) + "&submit=" + $(this).attr('name'),
       success: function(html) {
         $(row).before(html).remove();
