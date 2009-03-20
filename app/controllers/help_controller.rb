9da6659f79fd4754eaf4bc592cd59bc24d3ab13a
David Hayward <spidahman@gmail.com>
2009-03-20 12:02:50 +0000

    Added the help page/images for the measurements section.

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,6 +1,10 @@
 class HelpController < ApplicationController
   before_filter :login_required
 
+  def measurements
+    @activemenuitem = 'menu-measurements'
+  end
+
   def weights
     @activemenuitem = 'menu-weights'
   end
